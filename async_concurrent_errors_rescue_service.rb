0fdf8bbcfc55e10fd37b263e878d7e9b572440a8
maikhel <mlecicki@gmail.com>
2024-09-27 17:48:24 +0200

    Rescue inside future in case of API error

--- async_concurrent_errors_rescue_service.rb
+++ async_concurrent_errors_rescue_service.rb
@@ -6,50 +6,54 @@ class AsyncConcurrentErrorsRescueService
   API_ENDPOINT = 'https://api.chucknorris.io/jokes/random'.freeze
 
   def self.call(requests_count)
-    jokes = Concurrent::Array.new
+    futures = []
+    requests_count.times { futures << future_with_retry(3) }
 
+    jokes = Concurrent::Promises.zip(*futures).value
+
+    jokes.compact
+  end
+
+  def self.future_with_retry(attempts = 3)
     requester = DataRequester.new(API_ENDPOINT)
 
-    while jokes.size < requests_count
-      future = Concurrent::Promises.future { requester.call }
-
-      begin
-        jokes << future.value!
-      rescue ApiError => e
-        puts "Error happened in api call: #{e}"
+    Concurrent::Promises.future do
+      requester.call
+    end.rescue do |error|
+      if attempts > 1
+        future_with_retry(attempts - 1)
+      else
+        nil
       end
     end
-
-    puts "Successes count: #{jokes.size}"
-    jokes
-  end
-end
-
-class ApiError < StandardError; end;
-class DataRequester
-  def initialize(url)
-    @url = url
   end
 
-  def call
-    raise ApiError, 'ERROR' if rand > 0.8 # 80% chance of success
+  class ApiError < StandardError; end;
+  class DataRequester
+    def initialize(url)
+      @url = url
+    end
 
-    response = make_request
+    def call
+      raise ApiError, 'ERROR' if rand > 0.8 # 80% chance of success
 
-    parse_response(response)
-  end
+      response = make_request
 
-  def make_request
-    uri = URI(@url)
-    response = Net::HTTP.get_response(uri)
-    response.body if response.is_a?(Net::HTTPSuccess)
-  end
+      parse_response(response)
+    end
 
-  def parse_response(response)
-    json_data = JSON.parse(response)
-    json_data['value'] if json_data.key?('value')
-  rescue JSON::ParserError => e
-    puts "Error parsing JSON response: #{e.message}"
-    nil
+    def make_request
+      uri = URI(@url)
+      response = Net::HTTP.get_response(uri)
+      response.body if response.is_a?(Net::HTTPSuccess)
+    end
+
+    def parse_response(response)
+      json_data = JSON.parse(response)
+      json_data['value'] if json_data.key?('value')
+    rescue JSON::ParserError => e
+      puts "Error parsing JSON response: #{e.message}"
+      nil
+    end
   end
 end
